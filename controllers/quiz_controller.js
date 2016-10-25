a8aa275d577d0bc0b002bd8db3f5f222510b7e26
Ricardo Gomez <ricar.gomez55@gmail.com>
2016-10-25 09:08:03 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,11 @@
+exports.question = function(res,res){
+	res.render('quizes/question',{pregunta: 'Capital de Italia'});
+};
+
+exports.answer = function(req,res){
+	if(req.query.respuesta === 'Roma'){
+		res.render('quizes/answer',{respuesta: 'Correcto'});
+	}else{
+		res.render('quizes/answer',{respuesta: 'Incorrecto'});
+	}
+};
