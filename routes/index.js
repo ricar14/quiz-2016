0376db002349c8891a9c02de945f83dd1301420f
Ricardo Gomez <ricar.gomez55@gmail.com>
2016-11-21 08:01:17 +0100

    Login

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz-2016',errors: [] });
@@ -10,6 +11,11 @@ router.get('/', function(req, res, next) {
 
 //Autoload de comandos con: quizId
 router.param('quizId',quizController.load); //autoload: quizId
+
+// Definicion de rutas de sesion
+router.get('/login', sessionController.new);   // formulario login
+router.post('/login', sessionController.create);  // crear sesion
+router.get('/logout', sessionController.destroy);  // destruir sesion
 
 // Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
